a03ac4c7c9161d1d3d5a3057c319dc90c54f5504
Alex Green <alexgreen0606@gmail.com>
2024-10-05 00:56:45 -0500

    removed unneeded package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
@@ -11,15 +10,7 @@ export default defineConfig({
       ...mdx({
         providerImportSource: '@mdx-js/react'
       })
-    },
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/docs/strengths.csv',
-          dest: ''
-        }
-      ]
-    })
+    }
   ],
   base: "/Resume"
 })
