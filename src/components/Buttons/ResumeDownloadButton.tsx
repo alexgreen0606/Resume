4824877d503e2ac2504a2637e3c54a5ca3b1628c
Alex Green <alexgreen0606@gmail.com>
2024-10-08 22:47:58 -0500

    fixed resume download

--- src/components/Buttons/ResumeDownloadButton.tsx
+++ src/components/Buttons/ResumeDownloadButton.tsx
@@ -1,5 +1,4 @@
 import { DocumentDownload } from "@carbon/icons-react"
-import ResumePdf from '../../docs/Resume.pdf'
 import { useTheme } from "../../styles/ThemeContext";
 import CustomButton from "./CustomButton";
 import { getIconSizeStyles } from "../../utils/sizeUtils";
@@ -16,7 +15,7 @@ const ResumeDownloadButton: React.FC<ResumeDownloadButtonProps> = ({ size = 'med
     const iconSize = getIconSizeStyles(size === 'medium' ? typography.button.fontSize : typography.smallButton.fontSize)
 
     return (
-        <a href={ResumePdf} download='Alexander-Green-Resume' target='_blank' className={margins ? 'tinyVerticalMargins' : ''}>
+        <a href="/Resume.pdf" download='Alexander-Green-Resume' target='_blank' className={margins ? 'tinyVerticalMargins' : ''}>
             <CustomButton type='secondary' size={size}>
                 <DocumentDownload size={iconSize} className='tinyRightMargin' />Download Resume PDF
             </CustomButton>
